c3216f282eef6ae85e64244a226bf42b6906f2b4
Camilo Arango <carango@crnova.com>
2024-10-04 10:20:30 -0500

    Add new fields to input

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,109 @@
-import Image from "next/image";
+"use client";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    document.getElementById("contactInfo")?.addEventListener("click", () => {
+      const street = (document.getElementById("street") as HTMLInputElement)
+        ?.value;
+      const city = (document.getElementById("city") as HTMLInputElement)?.value;
+      const zip = (document.getElementById("zip") as HTMLInputElement)?.value;
+      const success = document.getElementsByClassName("success");
+      const error = document.getElementsByClassName("error");
+      const isNumeric = /^\d+$/.test(zip); // test if zip code is numeric
+
+      // if all fields are filled out and zip code is numeric
+      if (street && city && zip && isNumeric) {
+        //@ts-expect-error fixing for deployment
+        success.style.display = "block";
+        //@ts-expect-error fixing for deployment
+
+        error.style.display = "none";
+      } else {
+        //@ts-expect-error fixing for deployment
+        error.style.display = "block";
+        //@ts-expect-error fixing for deployment
+
+        success.style.display = "none";
+      }
+    });
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <head>
+        <title>Simple Form</title>
+      </head>
+      <body>
+        <div
+          style={{
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "300px",
+            padding: "10px",
+            border: "1px solid black",
+            marginBottom: "8px",
+          }}
+        >
+          <form id="contactInfo">
+            <div>
+              <label htmlFor="street">Street</label>
+              <input
+                id="street"
+                placeholder="Street..."
+                maxLength={80}
+                title="Street"
+              />
+            </div>
+            <div>
+              <label htmlFor="city">City</label>
+              <input
+                id="city"
+                placeholder="City..."
+                maxLength={80}
+                title="City"
+              />
+            </div>
+            <div>
+              <label htmlFor="zip">Zip Code</label>
+              <input
+                id="zip"
+                placeholder="Zip Code..."
+                maxLength={80}
+                title="Zip Code"
+              />
+            </div>
+            <div>
+              <label htmlFor="newsletter">Subscribe to newsletter</label>
+              <input id="newsletter" type="checkbox" />
+            </div>
+            <div>
+              <label htmlFor="contact">Contact</label>
+              <input id="contact" type="radio" />
+            </div>
+            <div>
+              <label htmlFor="comments">Comments</label>
+              <textarea name="comments" id="comments"></textarea>
+            </div>
+            <div>
+              <label htmlFor="country">Country</label>
+              <select name="country" id="country">
+                <option value="US">United States</option>
+                <option value="MX">Mexico </option>
+                <option value="CA">Canada</option>
+              </select>
+            </div>
+            <button id="submit" name="submit" type="button">
+              Save Contact Info
+            </button>
+            <a href="#">Some link</a>
+          </form>
+          <span className="success">Success</span>
+          <span className="error">Error</span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </body>
     </div>
   );
 }
