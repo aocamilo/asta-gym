3bb058b49a2867755f8e9f5c40562bd31d4607ba
Camilo Arango <carango@crnova.com>
2024-12-31 15:56:10 -0500

    Missing resources page added

---
+++ app/components/MissingResources.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function MissingResources() {
+  return (
+    <div style={{ backgroundImage: "missing-background.png" }}>
+      <img src="missing-img.png" />
+    </div>
+  );
+}
