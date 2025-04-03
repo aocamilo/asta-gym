316c29e22c2de26943dcca1ba6f5e569fbbd7ca1
Camilo Arango <carango@crnova.com>
2025-04-03 13:47:03 -0500

    Revert changes made

--- app/components/Links.tsx
+++ app/components/Links.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 // Define a type for different link types
 type LinkItem = {
@@ -13,84 +13,84 @@ type LinkItem = {
 export default function LinksList() {
   // Sample links of various types
   const links: LinkItem[] = [
-    // {
-    //   id: 1,
-    //   url: "https://www.google.com",
-    //   label: "Google",
-    //   type: "website",
-    // },
-    // {
-    //   id: 2,
-    //   url: "mailto:example@email.com",
-    //   label: "Email Contact",
-    //   type: "mailto",
-    // },
-    // {
-    //   id: 3,
-    //   url: "tel:+1234567890",
-    //   label: "Phone Number",
-    //   type: "tel",
-    // },
-    // {
-    //   id: 4,
-    //   url: "sms:+1234567890",
-    //   label: "Send SMS",
-    //   type: "sms",
-    // },
-    // {
-    //   id: 5,
-    //   url: "javascript:alert('Hello World!')",
-    //   label: "JavaScript Alert Link",
-    //   type: "javascript",
-    // },
-    // {
-    //   id: 6,
-    //   url: "https://www.thiswebsitedoesnotexist123456789.com",
-    //   label: "Broken Website Link",
-    //   type: "broken",
-    // },
-    // {
-    //   id: 7,
-    //   url: "ftp://ftp.example.com/files",
-    //   label: "FTP Server",
-    //   type: "ftp",
-    // },
-    // {
-    //   id: 8,
-    //   url: "mailto:support@company.com",
-    //   label: "Support Email",
-    //   type: "mailto",
-    // },
-    // {
-    //   id: 9,
-    //   url: "tel:+9876543210",
-    //   label: "Customer Service",
-    //   type: "tel",
-    // },
+    {
+      id: 1,
+      url: "https://www.google.com",
+      label: "Google",
+      type: "website",
+    },
+    {
+      id: 2,
+      url: "mailto:example@email.com",
+      label: "Email Contact",
+      type: "mailto",
+    },
+    {
+      id: 3,
+      url: "tel:+1234567890",
+      label: "Phone Number",
+      type: "tel",
+    },
+    {
+      id: 4,
+      url: "sms:+1234567890",
+      label: "Send SMS",
+      type: "sms",
+    },
+    {
+      id: 5,
+      url: "javascript:alert('Hello World!')",
+      label: "JavaScript Alert Link",
+      type: "javascript",
+    },
+    {
+      id: 6,
+      url: "https://www.thiswebsitedoesnotexist123456789.com",
+      label: "Broken Website Link",
+      type: "broken",
+    },
+    {
+      id: 7,
+      url: "ftp://ftp.example.com/files",
+      label: "FTP Server",
+      type: "ftp",
+    },
+    {
+      id: 8,
+      url: "mailto:support@company.com",
+      label: "Support Email",
+      type: "mailto",
+    },
+    {
+      id: 9,
+      url: "tel:+9876543210",
+      label: "Customer Service",
+      type: "tel",
+    },
     {
       id: 10,
       url: "sms:+9876543210?body=Hello%20there",
       label: "Prefilled SMS",
       type: "sms",
     },
-    // {
-    //   id: 11,
-    //   url: "javascript:console.log('Testing console link')",
-    //   label: "Console Log Link",
-    //   type: "javascript",
-    // },
-    // {
-    //   id: 12,
-    //   url: "http://completelyfakeandbrokenlink.xyz",
-    //   label: "Another Broken Link",
-    //   type: "broken",
-    // },
-    // {
-    //   id: 13,
-    //   url: "https://asta-gym.vercel.app/links/doesnt",
-    //   label: "Internal broken link",
-    //   type: "broken",
-    // },
+    {
+      id: 11,
+      url: "javascript:console.log('Testing console link')",
+      label: "Console Log Link",
+      type: "javascript",
+    },
+    {
+      id: 12,
+      url: "http://completelyfakeandbrokenlink.xyz",
+      label: "Another Broken Link",
+      type: "broken",
+    },
+    {
+      id: 13,
+      url: "https://asta-gym.vercel.app/links/doesnt",
+      label: "Internal broken link",
+      type: "broken",
+    },
   ];
 
   // Function to render link with appropriate styling
@@ -157,14 +157,14 @@ export default function LinksList() {
 
   return (
     <Card className="w-full max-w-md mx-auto">
-      {/* <CardHeader>
+      <CardHeader>
         <CardTitle className="text-2xl font-bold text-center">
           Links Collection
         </CardTitle>
-      </CardHeader> */}
+      </CardHeader>
       <CardContent>
         <div className="space-y-4">
-          {/* <h2 className="text-lg font-semibold">All Types of Links</h2> */}
+          <h2 className="text-lg font-semibold">All Types of Links</h2>
           {links.map(renderLink)}
         </div>
       </CardContent>
